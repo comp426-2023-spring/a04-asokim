b399679471af9b0574531100af96bd849832c785
Anna Kim <asokim@email.unc.edu>
2023-04-14 23:45:06 -0400

    Update server.js

--- server.js
+++ server.js
@@ -8,3 +8,9 @@ const args = minimist(process.argv.slice(2));
 const port = args["port"] || 5000;
 const app = express();
 
+app.use(express.json());
+
+app.get('/app', (req, res) => {
+    res.status(200).send("200 OK");
+});
+
