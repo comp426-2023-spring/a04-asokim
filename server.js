9080d7a901677932134bafb45aba18e6c8984f4d
Anna Kim <asokim@email.unc.edu>
2023-04-14 23:52:53 -0400

    Fixed typo

--- server.js
+++ server.js
@@ -34,5 +34,5 @@ app.get('/app/rpsls/play', (req, res) => {
 
 //json requests
 app.post('/app/rps/play/', (req, res) => {
-	res.status(200).send(PlayRps(req.body.shot));
+	res.status(200).send(rps(req.body.shot));
 });
