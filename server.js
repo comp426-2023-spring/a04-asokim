5285ce55b05334a0d2ef32bc60110435b4b8b9dc
Anna Kim <asokim@email.unc.edu>
2023-04-14 23:45:31 -0400

    Add app.use

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const port = args["port"] || 5000;
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.get('/app', (req, res) => {
     res.status(200).send("200 OK");
